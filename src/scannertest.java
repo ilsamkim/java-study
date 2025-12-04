99cf770738c81c7e6a790acac530e9478b9360dd
김동진 <kimdj9790@naver.com>
2025-12-04 14:53:28 +0900

    scannertest 와 gitignore

--- src/scannertest.java
+++ src/scannertest.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class scannertest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 사용자로부터 이름을 입력받기
+        System.out.print("이름을 입력하세요: ");
+        String name = scanner.nextLine();
+
+        // TODO: 사용자로부터 나이를 입력받기
+        System.out.print("나이를 입력하세요: ");
+        int age = scanner.nextInt();
+
+        // TODO: 입력받은 값 출력
+        System.out.println("출력 결과:");
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+    }
 }
