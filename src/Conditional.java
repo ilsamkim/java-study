a1fe388554840990526a073f74c2563be0dbe8d2
김동진 <kimdj9790@naver.com>
2025-12-04 17:24:04 +0900

    조건문 문제

---
+++ src/Conditional.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("신호등 색상을 입력하세요 (초록불, 노란불, 빨간불): ");
+        String color = scanner.nextLine();
+        if (color.equals("초록불")) {
+            System.out.println("건너세요!");
+        } else if (color.equals("노란불")) {
+            System.out.println("주의하세요!");
+        } else if (color.equals("빨간불")) {
+            System.out.println("멈추세요!");
+        } else {
+            System.out.println("잘못된 입력입니다.");
+        }
+    }
+}
