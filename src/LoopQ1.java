7096ae2003a6ae55b840d7a387061fccdc76ae04
김동진 <kimdj9790@naver.com>
2025-12-04 19:39:13 +0900

    반복문 1

---
+++ src/LoopQ1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LoopQ1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("출력할 구구단 단수를 입력하세요 (2~9): ");
+        int o = scanner.nextInt();
+
+        // for문 활용하기
+        //for (int i = 1; i <= 9; i++) {
+        //    System.out.println(o + " x " + i + " = " + i * o);
+        //}
+
+        // while문 활용하기
+        int i = 1;
+        while (i <= 9) {
+            System.out.println(o + " x " + i + " = " + i * o);
+            i++;
+        }
+    }
+}
