b5cd1fda8c12e8d09edc42096ffefa0a6f8dc808
김동진 <kimdj9790@naver.com>
2025-12-26 14:19:39 +0900

    Q21

---
+++ src/flowchart/Example.java
@@ -0,0 +1,12 @@
+package flowchart;
+
+public class Example {
+    public static void main(String[] args) {
+        // Q21
+        int num = 1;
+        while (num <= 10) {
+            System.out.println(num);
+            num+=1;
+        }
+    }
+}
