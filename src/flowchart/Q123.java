728dfd44cded39da1a3f63688770a0493a48c5b7
김동진 <kimdj9790@naver.com>
2025-12-30 09:34:28 +0900

    Q123

---
+++ src/flowchart/Q123.java
@@ -0,0 +1,19 @@
+package flowchart;
+
+import java.util.Scanner;
+
+public class Q123 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("숫자를 입력하세요: ");
+            int num = sc.nextInt();
+
+            int i = 1;
+            while(i <= 9){
+                int result = i * num;
+                System.out.println(num + " X " + i + " = " + result);
+                i++;
+            }
+    }
+}
