a6181d6982535ba40bd96b4592bca14df645d121
김동진 <kimdj9790@naver.com>
2025-12-31 11:26:10 +0900

    이름 수정

--- src/flowchart/Q22.java
+++ src/flowchart/Q22.java
@@ -2,7 +2,7 @@ package flowchart;
 
 import java.util.Scanner;
 
-public class Q123 {
+public class Q22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
